7628c1e4d4945d5675096e7e414444d8f1db631b
devmarcs <devmarcelo.gus@gmail.com>
2023-11-20 11:48:16 -0300

    Criação da função que executa os sons para cada botão

--- script/main.js
+++ script/main.js
@@ -1,12 +1,27 @@
-function tocaSomPom() {
-    return document.querySelector('#som_tecla_pom').play();
+function tocaSom(idElementoAudio) {
+    return document.querySelector(idElementoAudio).play();
 }
 
-function tocaSomClap() {
-    return document.querySelector('#som_tecla_clap').play();
+//Pegou todos os elementos da classe .tecla 
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+//contador para contar a condição booleana do while
+
+
+for (contador = 0; contador < listaDeTeclas.length; contador +=1) {
+
+    //Salvando todos os elementos da classe tecla com índex do contador na const tecla
+    const tecla = listaDeTeclas[contador];
+
+    //Pego cada item da classlist do elemento da classe tecla
+    const instrumento =  tecla.classList[1]; 
+
+    //Acrescento uma concatenação da inicial do id do áudio com a segunda parte da lista de teclas (me pareceu uma gambiarra)
+    const idAudio = `#som_${instrumento}`
+    
+    //Ao clicar no botão no html chama a função anonima que contém a função que toca o som específico de cada tecla
+    tecla.onclick = function(){
+        tocaSom(idAudio)
+    };
 }
-
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
-
-
 
